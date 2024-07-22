ad0d5ad5bf88b64c647ac1b930492c111c791bb6
Ashray-Chaitanya <ashrayc1@gmail.com>
2024-07-22 23:51:20 +0530

    new

---
+++ insertesh.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template, request
+from flask_mysqldb import MySQL
+ 
+app = Flask(__name__)
+ 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Jett@h1k0'
+app.config['MYSQL_DB'] = 'new_schema'
+ 
+mysql = MySQL(app)
+ 
+@app.route('/')
+def form():
+    return render_template('sample.html')
+
+@app.route('/ufdate')
+def login():
+        cursor = mysql.connection.cursor()
+        for i in range(1,71):
+            cursor.execute(f"INSERT INTO credentials (USN, USERNAME, PASS) VALUES ('1bi22is{i}','user{i}','bit{i}');")
+        mysql.connection.commit()
+        cursor.close()
+        return f"Done!!"
+ 
+app.run(host='localhost', port=5000)
